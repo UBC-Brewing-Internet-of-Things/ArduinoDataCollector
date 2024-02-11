a0c0ed6da8f3d8bceb9a515104709b07a4d820f7
Owen <142843706+Rkayic@users.noreply.github.com>
2024-02-11 10:40:11 -0800

    Update Read-Data.ino
    Implemented read data function

--- ArduinoDataCollector-main/Read-Data.ino
+++ ArduinoDataCollector-main/Read-Data.ino
@@ -5,13 +5,35 @@ void setup(){
     Serial.begin(9600);
 }
 
+struct DataPacket {
+  char value;
+  int time;
+  String type;
+};
 
+// reads from serial buffer a char, integer and a string in that order. String is terminated with newline character.
 DataPacket readData(){
-    int incomingByte;
-    if(Serial.avalible() > 0){
-        incomingByte = Serial.Read();
-        
+    int integerSent;
+    char valueSent;
+    String typeSent;
+    if(serial.avalible() >= 3){
+        if((serial.peek() != NULL) && (valueSent == NULL)){
+            valueSent = Serial.read();
+        }
+        if((serial.peek() != NULL) && (integerSent == NULL)){
+            integerSent = Serial.read();
+        }
+        typeSent = Serial.read();
+        while(serial.peek() != '\n'){
+            typeSent += Serial.read();
+        }
+
+        struct DataPacket returnData;
+        returnData.time = integerSent;
+        returnData.value = valueSent;
+        returnData.type = typeSent;
+        return returnData;
     }
-    
+    return NULL;
 }
 
