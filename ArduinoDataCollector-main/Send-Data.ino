8565952874b573d7207cf28e696766785250e247
Owen <142843706+Rkayic@users.noreply.github.com>
2024-03-03 11:06:56 -0800

    Updated Send data, needs testing and reveiw

--- ArduinoDataCollector-main/Send-Data.ino
+++ ArduinoDataCollector-main/Send-Data.ino
@@ -1,2 +1,36 @@
 #include <WiFi.h>
+#include "Main.ino"
 
+char ssid[] = "myNetwork";          //  your network SSID (name)
+char pass[] = "myPassword";   // your network password
+
+int status = WL_IDLE_STATUS;
+IPAddress server(74,125,115,105);  // Google
+
+
+int initializeWifiSettings(){
+
+    int attempts = 0;
+    Serial.begin(9600);
+    Serial.println(ssid);
+
+    while((status = WL_IDLE_STATUS) && (attempts < 10)){
+        attempts++;
+        status = WiFi.begin(ssid, pass);
+    }
+
+    if (status == WL_CONNECTED) {
+        client.connect(server, 80);
+    }
+    return status;
+}   
+
+
+
+bool sendPacket(DataPacket input){
+    bool connected = client.connected();
+    if(connected){
+        client.write(input);
+    }
+    return connected;
+}
