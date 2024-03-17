2e0af2484c2498913cd57159778b083091ba8116
Owen <142843706+Rkayic@users.noreply.github.com>
2024-03-17 11:44:05 -0700

    Update Send-Data.ino
    updated write function to use string datatype and properly send both bytes of a integer for arduino systems

--- ArduinoDataCollector-main/Send-Data.ino
+++ ArduinoDataCollector-main/Send-Data.ino
@@ -30,9 +30,9 @@ int initializeWifiSettings(){
 bool sendPacket(DataPacket* input){
     bool connected = client.connected();
     if(connected && (DataPacket != nullptr)){ 
-        client.write((string)input->value); 
-        client.write(input->time);
-        client.write(input->type);
+        client.write(string (input->value)); 
+        client.write(input->time, 2);
+        client.write(string (input->type));
     }
     return connected;
 }
